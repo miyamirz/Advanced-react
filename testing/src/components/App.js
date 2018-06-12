ef56e7cc9b262a12fa8278d938c47a3a7c0fd467
Miya Mirza <Miya_Mirza@Miyas-MacBook-Pro.local>
2018-06-13 02:20:44 +0530

    1.Added routes 2.signin and signout button

--- testing/src/components/App.js
+++ testing/src/components/App.js
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CommentBox from './CommentBox';
+import { Route, Link } from 'react-router-dom';
 import CommentList from './CommentList';
+import * as actions from '../actions';
 
-export default ()  => {
-  return (
-    <div>
-      <CommentBox />
-      <CommentList />
-    </div>
-  );
-};
+class App extends Component {
+	renderButton() {
+		if (this.props.auth) {
+			return (
+				<button onClick={() => this.props.changeAuth(false)}>
+					Sign out
+				</button>
+			);
+		} else {
+			return (
+				<button onClick={() => this.props.changeAuth(true)}>
+					Sign in
+				</button>
+			);
+		}
+	}
+	renderHeader() {
+		return (
+			<ul>
+				<li>
+					<Link to="/">Home</Link>
+				</li>
+				<li>
+					<Link to="post">Post</Link>
+				</li>
+				<li>{this.renderButton()}</li>
+			</ul>
+		);
+	}
+	render() {
+		return (
+			<div>
+				{this.renderHeader()}
+				<Route path="/post" component={CommentBox} />
+				<Route path="/" exact component={CommentList} />
+			</div>
+		);
+	}
+}
+function mapStateToProps(state) {
+	return { auth: state.auth };
+}
+export default connect(
+	mapStateToProps,
+	actions
+)(App);
--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -1,34 +1,42 @@
-import React, { Component} from 'react';
-import { connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 class CommentBox extends Component {
-  state = { comment: ''};
+	state = { comment: '' };
 
-  handleChange = event => {
-    this.setState({ comment: event.target.value });
-  };
+	handleChange = event => {
+		this.setState({ comment: event.target.value });
+	};
 
-  handleSubmit = event => {
-    event.preventDefault();
+	handleSubmit = event => {
+		event.preventDefault();
+		this.setState({ comment: '' });
+		this.props.saveComment(this.state.comment);
+	};
 
-    this.setState({ comment: '' });
-  };
-
-  render() {
-    return(
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <h4>Add a Comment</h4>
-          <textarea onChange={this.handelChange} value={this.state.comment}/>
-          <div>
-            <button>Submit Comment</button>
-          </div>
-        </form>
-        <button onClick={this.props.fetchComments}>Fetch Comments</button>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<form onSubmit={this.handleSubmit}>
+					<h4>Add a Comment</h4>
+					<textarea
+						onChange={this.handleChange}
+						value={this.state.comment}
+					/>
+					<div>
+						<button>Submit Comment</button>
+					</div>
+				</form>
+				<button onClick={this.props.fetchComments}>
+					Fetch Comments
+				</button>
+			</div>
+		);
+	}
 }
 
-export default connect(null, actions)(CommentBox);
+export default connect(
+	null,
+	actions
+)(CommentBox);

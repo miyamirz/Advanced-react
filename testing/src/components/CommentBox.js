3853345d31c63236429449158c023e3f7d33c86e
Miya Mirza <Miya_Mirza@Miyas-MacBook-Pro.local>
2018-06-14 00:19:24 +0530

    Refactored the code and added to higher order component

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -1,23 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import requireAuth from './requireAuth';
 import * as actions from '../actions';
 
 class CommentBox extends Component {
 	state = { comment: '' };
-
-	componentDidMount() {
-		if (!this.props.auth) {
-			this.shouldNavigateAway();
-		}
-	}
-	componentDidUpdate() {
-		if (!this.props.auth) {
-			this.shouldNavigateAway();
-		}
-	}
-	shouldNavigateAway() {
-		this.props.history.push('/');
-	}
 
 	handleChange = event => {
 		this.setState({ comment: event.target.value });
@@ -50,10 +37,7 @@ class CommentBox extends Component {
 	}
 }
 
-function mapStateToProps(state) {
-	return { auth: state.auth };
-}
 export default connect(
-	mapStateToProps,
+	null,
 	actions
 )(CommentBox);
---
+++ testing/src/components/requireAuth.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default ChildComponent => {
+	class ComposedComponent extends Component {
+		componentDidMount() {
+			this.shouldNavigateAway();
+		}
+		componentDidUpdate() {
+			this.shouldNavigateAway();
+		}
+		shouldNavigateAway() {
+			this.prop.history.push('/');
+		}
+		render() {
+			return <ChildComponent />;
+		}
+	}
+
+	function mapStateToProps(state) {
+		return { auth: state.auth };
+	}
+	return connect(mapStateToProps)(ComposedComponent);
+};
